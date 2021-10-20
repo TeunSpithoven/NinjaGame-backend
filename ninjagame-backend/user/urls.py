9c947fe6ebfad9d3ee228bac9e013f9e0a0b1c22
Teun Spithoven <teun.spithoven@gmail.com>
2021-10-20 15:57:19 +0200

    Revert "created django app with http requests"
    This reverts commit ecf35fc6dc059a7d86164814e2affc75a4eff898.

--- ninjagame-backend/user/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = {
-    path('create/', views.create_user),
-    path('getall/', views.get_user_all),
-    path(r'get/(?P<username>\w+)$', views.get_user_byusername),
-}
