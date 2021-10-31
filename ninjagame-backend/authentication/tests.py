cdd1108b1dca31f5f44e2edfc133cf8b20fadc05
Teun Spithoven <teun.spithoven@gmail.com>
2021-10-31 16:46:43 +0100

    added dockerfile

--- ninjagame-backend/authentication/tests.py
+++ ninjagame-backend/authentication/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.contrib.auth.models import User
+from django.contrib import auth
+from django.test import Client
+from django.urls import reverse
+
+def test_login_valid():
+    username = {'username': 'bob','password': 'bobbobbob'}
+    user = User.objects.create_user(**username)
+    C = Client()
+    r = C.post(reverse('login'), user)
+    su = auth.get_user(r.wsgi_request)
+
+    print(username)
+    print(r)
+    print(su)
+    print(su.is_authenticated)
+    print(r.wsgi_request.user)
+    print(r.wsgi_request.user.is_authenticated)
--- ninjagame-backend/games/serializers.py
+++ ninjagame-backend/games/serializers.py
@@ -10,10 +10,9 @@ class GameSerializer(serializers.ModelSerializer):  # create class to serializer
         model = Game
         fields = ('start_datetime', 'end_datetime', 'score', 'user')
 
-
 class UserSerializer(serializers.ModelSerializer):  # create class to serializer user model
     movies = serializers.PrimaryKeyRelatedField(many=True, queryset=Game.objects.all())
 
     class Meta:
-        model = User
         fields = ('id', 'username', 'games')
+        model = User
