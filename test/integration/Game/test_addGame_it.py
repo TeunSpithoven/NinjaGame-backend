0e03b459539f02817928a524ca956ca4a41d89fd
Teun Spithoven <teun.spithoven@gmail.com>
2022-01-02 20:45:10 +0100

    addGame and getGames integration tests

---
+++ test/integration/Game/test_addGame_it.py
@@ -0,0 +1,34 @@
+from django.test import TestCase
+# from django.urls import reverse
+from games.models import Game
+
+
+class RegisterIntegrationTestCase(TestCase):
+    def setUp(self):
+        register_data = {
+            "username": "refreshTestUser",
+            "password": "passwordhutser1!",
+            "password2": "passwordhutser1!"
+        }
+        self.client.post('/auth/register/', register_data)
+        login_data = {
+            "username": "refreshTestUser",
+            "password": "passwordhutser1!"
+        }
+        response = self.client.post('/auth/token/', login_data)
+        self.accessToken = response.data["access"]
+        self.bearer = {'HTTP_AUTHORIZATION':'Bearer {}'.format(self.accessToken)}
+
+    def test_addGame(self):
+        game_data = {
+            "start_datetime": "2019-12-10 14:43:35.542195",
+            "end_datetime": "2019-12-10 14:43:35.542195",
+            "score": 69,
+            "user": 1
+        }
+        response = self.client.post('/games/', game_data, **self.bearer)
+        self.assertEqual(response.status_code, 201)
+
+        game = Game.objects.get(user=1)
+        self.assertIsNotNone(game)
+        self.assertEqual(game.score, 69)
---
+++ test/integration/Game/test_getGames_it.py
@@ -0,0 +1,44 @@
+from django.test import TestCase
+# from django.urls import reverse
+from games.models import Game
+
+
+class RegisterIntegrationTestCase(TestCase):
+    def setUp(self):
+        register_data = {
+            "username": "refreshTestUser",
+            "password": "passwordhutser1!",
+            "password2": "passwordhutser1!"
+        }
+        self.client.post('/auth/register/', register_data)
+        login_data = {
+            "username": "refreshTestUser",
+            "password": "passwordhutser1!"
+        }
+        response = self.client.post('/auth/token/', login_data)
+        self.accessToken = response.data["access"]
+        self.bearer = {'HTTP_AUTHORIZATION':'Bearer {}'.format(self.accessToken)}
+
+        game_data = {
+            "start_datetime": "2019-12-10 14:43:35.542195",
+            "end_datetime": "2019-12-10 14:43:35.542195",
+            "score": 69,
+            "user": 1
+        }
+        response = self.client.post('/games/', game_data, **self.bearer)
+
+    def test_getGames(self):
+        response = self.client.get('/games/', **self.bearer)
+        self.assertEqual(response.status_code, 200)
+        results = response.data["results"]
+
+        l1 = len(results[0]["start_datetime"])
+        l2 = len(results[0]["end_datetime"])
+ 
+        results[0]["start_datetime"] = results[0]["start_datetime"][:l1-1]
+        results[0]["end_datetime"] = results[0]["end_datetime"][:l2-1]
+
+        self.assertEqual(results[0]["start_datetime"], "2019-12-10T14:43:35.542195")
+        self.assertEqual(results[0]["end_datetime"], "2019-12-10T14:43:35.542195")
+        self.assertEqual(results[0]["score"], 69)
+        self.assertEqual(results[0]["user"], "refreshTestUser")
